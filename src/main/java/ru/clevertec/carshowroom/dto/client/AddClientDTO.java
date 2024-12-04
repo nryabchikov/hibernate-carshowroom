f91f4786ee919e5886ac476900e35bf236ef3ee7
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-12-05 00:20:27 +0300

    added carDTO classes

---
+++ src/main/java/ru/clevertec/carshowroom/dto/client/AddClientDTO.java
@@ -0,0 +1,19 @@
+package ru.clevertec.carshowroom.dto.client;
+
+import java.util.Date;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddClientDTO {
+    private String name;
+    private Set<String> contacts;
+    private Date dateOfRegistration;
+}
